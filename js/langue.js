304ff3a896035fa56944fa871db44788fb0d9671
aldsanms <aldryck.sanchez@viacesi.fr>
2023-07-13 00:28:44 +0200

    maj

---
+++ js/langue.js
@@ -0,0 +1,55 @@
+
+var lan= 0;
+
+var baniere={
+
+    i1: ["Fr","En"],
+
+    l1: ["Accueil"],
+    l2: ["Menu"],
+    l3: ["À propos"],
+    l4: ["Contact","Contact"]
+};
+
+var all={
+    nom: ["Vert olive pizza"],
+    num: ["+33 620 081 544"],
+    adresseEmail: ["vertolivepizza@gmail.com"],
+    adresse: ["1 avenue Michel Rocard"],
+    ville: ["Villemur-sur-Tarn"],
+    cp: ["31340"],
+    pays: ["France"]
+};
+
+var ReseauxSociaux={
+    facebook: ["https://www.facebook.com/people/Vert-olive-pizza/100063702392327/?sk=about"]
+};
+
+
+var accueil={
+    t1:[""],
+
+    i1:["La pizza autrement"],
+    i2:["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu leo tortor. Nunc eleifend malesuada magna, a sollicitudin tortor luctus ac. Aenean eget eros sit amet sapien commodo viverra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris scelerisque dapibus justo a aliquet. Cras non commodo tellus. In dignissim, quam quis euismod iaculis, risus nisi commodo elit, ac tincidunt risus nibh vel nulla. Etiam rutrum, lorem at accumsan dictum, quam libero accumsan magna, et auctor nibh lectus et sapien."],
+};
+
+var contact={
+    t1:[""],
+
+    i1:["Contactez "+ all.nom],
+    i2:["Téléphone"],
+};
+
+var footer={
+
+    i1: ["Réseaux sociaux"],
+
+    i2: ["Contact"],
+
+    i5: ["Navigation"],
+    l1: ["Accueil"],
+
+    i6: ["&copy; 2023 - "+ all.nom +" - Tous droits réservés"],
+    i7: ["Site créer par"],
+    
+};
